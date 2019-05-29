3b10cd6a4317aef3536caac51b942086ca1d7216
rmaxer1408 <rmaxer1408@gmail.com>
2019-05-30 02:23:23 +0300

    header was added

--- gulpfile.js
+++ gulpfile.js
@@ -8,82 +8,82 @@ const rename 	  = require('gulp-rename');
 const autoprefixer= require('gulp-autoprefixer');
 const sourcemaps  = require('gulp-sourcemaps');
 
-// Static server
-gulp.task('server', function() {
-    browserSync.init({
-        server: {
-        	port: 9000,
-            baseDir: "build"
-        }
-    });
+	// Static server
+	gulp.task('server', function() {
+	    browserSync.init({
+	        server: {
+	        	port: 9000,
+	            baseDir: "build"
+	        }
+	    });
 
-    gulp.watch('build/**/*').on('change', browserSync.reload);
-});
+	    gulp.watch('build/**/*').on('change', browserSync.reload);
+	});
 
-// Pug
-gulp.task('templates:compile', function buildHTML() {
-  return gulp.src('source/templates/index.pug')
-  .pipe(pug({
-    pretty: true
-  }))
-  .pipe(gulp.dest('build'))
-});
+	// Pug
+	gulp.task('templates:compile', function buildHTML() {
+	  return gulp.src('source/templates/index.pug')
+	  .pipe(pug({
+	    pretty: true
+	  }))
+	  .pipe(gulp.dest('build'))
+	});
 
-// Sass
-gulp.task('styles:compile', function () {
-  return gulp.src('source/styles/main.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(autoprefixer({
-            browsers: ['last 10 versions'],
-            cascade: true
-        }))
-    .pipe(rename('main.min.css'))
-    .pipe(gulp.dest('build/css'));
-});
+	// Sass
+	gulp.task('styles:compile', function () {
+	  return gulp.src('source/styles/main.scss')
+	    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+	    .pipe(autoprefixer({
+	            browsers: ['last 10 versions'],
+	            cascade: true
+	        }))
+	    .pipe(rename('main.min.css'))
+	    .pipe(gulp.dest('build/css'));
+	});
 
-// Sprite
-gulp.task('sprite', function (cb) {
-  const spriteData = gulp.src('source/images/*.png').pipe(spritesmith({
-    imgName: 'sprite.png',
-    imgPath: '../images/*.png',
-    cssName: 'sprite.scss'
-  }));
+	// Sprite
+	gulp.task('sprite', function (cb) {
+	  const spriteData = gulp.src('source/images/*.png').pipe(spritesmith({
+	    imgName: 'sprite.png',
+	    imgPath: '../images/*.png',
+	    cssName: 'sprite.scss'
+	  }));
 
-  spriteData.img.pipe(gulp.dest('build/images/'));
-  spriteData.css.pipe(gulp.dest('source/styles/global/'));
-  cb();
-});
+	  spriteData.img.pipe(gulp.dest('build/images/'));
+	  spriteData.css.pipe(gulp.dest('source/styles/global/'));
+	  cb();
+	});
 
-// Delete
-gulp.task('clean', function del(cb) {
-	return rimraf('build', cb);
-});
+	// Delete
+	gulp.task('clean', function del(cb) {
+		return rimraf('build', cb);
+	});
 
-// Copy fonts
-gulp.task('copy:fonts', function() {
-	return gulp.src('./source/fonts/**/*.*')
-	.pipe(gulp.dest('build/fonts'));
-});
+	// Copy fonts
+	gulp.task('copy:fonts', function() {
+		return gulp.src('./source/fonts/**/*.*')
+		.pipe(gulp.dest('build/fonts'));
+	});
 
-// Copy images
-gulp.task('copy:images', function() {
-	return gulp.src('./source/images/**/*.*')
-	.pipe(gulp.dest('build/images'));
-});
+	// Copy images
+	gulp.task('copy:images', function() {
+		return gulp.src('./source/images/**/*.*')
+		.pipe(gulp.dest('build/images'));
+	});
 
-// Copy
-gulp.task('copy', gulp.parallel('copy:fonts', 'copy:images'));
+	// Copy
+	gulp.task('copy', gulp.parallel('copy:fonts', 'copy:images'));
 
-// Watchers
-gulp.task('watch', function() {
-	gulp.watch('source/templates/**/*.pug', gulp.series('templates:compile'));
-	gulp.watch('source/styles/**/*.scss', gulp.series('styles:compile'));
-});
+	// Watchers
+	gulp.task('watch', function() {
+		gulp.watch('source/templates/**/*.pug', gulp.series('templates:compile'));
+		gulp.watch('source/styles/**/*.scss', gulp.series('styles:compile'));
+	});
 
-// Default
-gulp.task('default', gulp.series(
-	'clean',
-	gulp.parallel('templates:compile','styles:compile', 'sprite', 'copy' ),
-	gulp.parallel('watch', 'server')
-	)
-);
+	// Default
+	gulp.task('default', gulp.series(
+		'clean',
+		gulp.parallel('templates:compile','styles:compile', 'sprite', 'copy' ),
+		gulp.parallel('watch', 'server')
+		)
+	);
